2359e6a19c086bdaf50b7da2937d0709f17057a1
p1armod <parmodrns@gmail.com>
2025-12-28 11:50:15 +0530

    Add RealtimeUpdatesService for communication between websocket and WikimediaStreamProcessor

---
+++ src/main/java/org/example/Service/RealtimeUpdatesService.java
@@ -0,0 +1,18 @@
+package org.example.Service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.Model.EditCountEvent;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RealtimeUpdatesService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void publish(EditCountEvent event){
+        messagingTemplate.convertAndSend("topic/edits", event);
+    }
+}

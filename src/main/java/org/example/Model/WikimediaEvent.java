8b2852fe91faf6abb72ff2ab87b086dc7e4604c4
p1armod <parmodrns@gmail.com>
2026-01-01 23:14:34 +0530

    Add wikimediaEvent DTO

---
+++ src/main/java/org/example/Model/WikimediaEvent.java
@@ -0,0 +1,9 @@
+package org.example.Model;
+
+public class WikimediaEvent {
+    private String wiki;
+    private boolean bot;
+    private boolean minor;
+    private long eventTime;
+    private int editSize;
+}

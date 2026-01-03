d5620aa22bc9030b2ffda15e2e84dd1dd8b9e0e4
p1armod <parmodrns@gmail.com>
2026-01-03 15:54:30 +0530

    Update DelayMetricDTO to have sum and count

--- src/main/java/org/example/Model/DelayMetricDTO.java
+++ src/main/java/org/example/Model/DelayMetricDTO.java
@@ -2,5 +2,10 @@ package org.example.Model;
 
 public class DelayMetricDTO {
     public long maxDelay;
-    public long avgDelay;
+    public long sum;
+    public long count;
+
+    public double getAvgDelay() {
+        return count == 0 ? 0 : (double)sum / count;
+    }
 }

586ceb79599929afee17f30538b6f87b6bdec0c4
p1armod <parmodrns@gmail.com>
2026-01-04 11:29:14 +0530

    Add CountPair DTO

---
+++ src/main/java/org/example/Model/CountPair.java
@@ -0,0 +1,13 @@
+package org.example.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountPair {
+    private long first;
+    private long second;
+}

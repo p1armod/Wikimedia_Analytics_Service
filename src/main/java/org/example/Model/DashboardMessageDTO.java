b5bcc563782607fe5e4f2d3a386c845db7c6cd32
p1armod <parmodrns@gmail.com>
2026-01-06 14:31:51 +0530

    Update DashboardMessageDTO to have constructors, getters and setters

--- src/main/java/org/example/Model/DashboardMessageDTO.java
+++ src/main/java/org/example/Model/DashboardMessageDTO.java
@@ -1,5 +1,12 @@
 package org.example.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DashboardMessageDTO<T> {
     public String message;
     public T data;
